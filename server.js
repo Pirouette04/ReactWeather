1b25b57a8a1784212884e7f0118aba94f3585fbf
Pirouette04 <chantook@Chantals-MacBook-Pro.local>
2017-02-22 15:36:38 -0500

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+if(req.headers['x-fowarded-proto'] == "http"){
+  next();
+} else {
+  res.redirect('https://' + req.hostname + req.url);
+}
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log("Server On");
+app.listen(PORT, function(){
+  console.log("Server On port " + PORT);
 })
